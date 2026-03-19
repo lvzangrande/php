7f5aa57bc053e10f1f342d8c7220e4e2d5162ce1
lvzangrande <zangrande360@gmail.com>
2026-03-19 14:09:32 -0300

    Conhecendo operadores aritiméticos em php

---
+++ Lógica/curso_em_video/operadores_aritimeticos/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//+ adição
+//. concatenação
+
+$r = "2" + "2";
+var_dump($r);//4 inteiro, devido ao sinal "+" as strings "2" foram convertidas para int
+
+/*
+    + Adição
+    - Substração
+    * Multiplicação
+    ** Exponenciaçâo
+    / Divisão
+    % Módulo (Resto de divisão)
+*/
+?>
