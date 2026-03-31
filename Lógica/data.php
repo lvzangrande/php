097baab0297ee0f05763a9f54a1fc1683725b729
Lucas Zangrande <zangrande360@gmail.com>
2026-03-31 17:05:35 -0300

    arquivos php do site

---
+++ Lógica/data.php
@@ -0,0 +1,34 @@
+<?php
+$categorias = [
+    'hardware' => 'Hardware',
+    'perifericos' => 'Periféricos'
+];
+
+$produtos_base = [
+    [
+        'id'=>1,
+        'nome'=> 'mouse rgb',
+        'preco'=> 129.90,
+        'categoria'=> 'periférico',
+        'descricao'=>'Mouse gamer de alta precisão com sensor óptico avançado e iluminação RGB personalizável. Ideal para jogos competitivos, oferece resposta rápida e conforto mesmo em longas sessões.',
+       'imagem'=> 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRo56DvM7ebte7sIcNC6IJ7orhtJr_ZvrLjuU3KEWHVc7teiwhkxiOWFePWLmcKD5YupQuv-1abDzKbyxYbkdQ8hvwyd0SzH5unO2PsHxtcnyh_bkmJuxu01g',
+    ],
+    [
+        'id'=>2,
+        'nome'=> 'Teclado mecânico rgb',
+        'preco'=> 249.90,
+        'categoria'=> 'periférico',
+        'descricao'=>'Teclado mecânico com switches de alta durabilidade e iluminação RGB dinâmica. Proporciona maior velocidade de resposta, precisão nos comandos e uma experiência imersiva para qualquer jogo.',
+        'imagem'=> 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSljZr9dIOfwTrEYAkivMpPeV7JYswk13DLltWKmYNpgHMiGbPrap6n8WYYkgz8xZEPL7nCIAKFTjmj7ZQFt8D9nINBiJhGN1YM1bwJgAA623ZJrf7KcHFZCg',
+    ]
+    ,
+    [
+        'id'=>3,
+        'nome'=> 'Headset gamer 7.1',
+        'preco'=> 199.90,
+        'categoria'=> 'periférico',
+        'descricao'=>'Headset com som surround 7.1 que garante imersão total nos jogos. Possui microfone com cancelamento de ruído e almofadas confortáveis para uso prolongado.',
+       'imagem'=> 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTX7M7Ip19Oc5ttOfoX64316QD1KiT-sVxTYtM-D6IMaFP8f4VNFutNygTJRTcHd6B1mgCD8sB0A-hR5idGYUGrPsvl6KlPrtqkQ6mD5UVedQHict7JpXVB7qs',
+    ]
+];
+?>
